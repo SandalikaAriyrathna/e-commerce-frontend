a94a6535ddbd3f6337b5f14f9bfa909f697b73ad
SandalikaAriyrathna <dilsandu96@gmail.com>
2024-05-11 15:08:42 +0530

    update

---
+++ src/_mock/account.js
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------
+
+const account = {
+    displayName: 'Mihiran Jayasekara',
+    email: 'demo@minimals.cc',
+    photoURL: '/assets/images/avatars/avatar_default.jpg',
+    role: 'Seller'
+};
+
+export default account;
